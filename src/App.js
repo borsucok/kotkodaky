95fa6900c04f999b6cb5cbe425ea1dbf4c75afab
Ladislav Perduk <ladislav.perduk@gmail.com>
2017-12-08 19:13:50 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Grid from "./Grid";
+
+class App extends Component {
+  render() {
+    return (
+        <Grid/>
+    );
+  }
+}
+
+export default App;
