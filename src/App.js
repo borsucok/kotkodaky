7463e361f95be8c6f83ae85fae000575efc39507
Michal Hricisin <hricisin@bart.sk>
2017-12-08 19:25:32 +0100

    Doplnenie API a boardu

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Grid from "./Grid";
+import API from './API';
 
 class App extends Component {
   render() {
